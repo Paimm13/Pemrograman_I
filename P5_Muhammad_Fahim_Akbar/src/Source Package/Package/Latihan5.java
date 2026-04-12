23434d34a29660ad3c56f70c6a05340f7e405fc4
Paimm13 <fahim.akbar03pagi@gmail.com>
2026-04-12 20:47:19 +0700

    git commit

--- P5_Muhammad_Fahim_Akbar/src/Source Package/Package/Latihan5.java
+++ P5_Muhammad_Fahim_Akbar/src/Source Package/Package/Latihan5.java
@@ -1,6 +1,39 @@
+import javax.swing.text.StyledEditorKit;
 import java.util.Scanner;
+import java.io.*;
 public class Latihan5 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pilihanPesanan;
+        boolean daftarMenu = true;
 
+        while (daftarMenu) {
+            System.out.println("Selamat Datang di Cafe Taria\n");
+            System.out.println("Silahkan Pilih Daftar Menu\n1. Makanan\n2. Minuman\n0. Keluar");
+            System.out.print("Masukan Pilihan : ");
+            pilihanPesanan = input.nextInt();
+
+            if (pilihanPesanan == 1) {
+                System.out.println("\nDaftar Menu Makanan");
+                System.out.println("1. Nasi Goreng\n2. Mie Goreng\n3. Kentang Goreng");
+                System.out.println("Ketik '0' jika ingin melihat daftar minuman");
+                int kembaliMakanan;
+                kembaliMakanan = input.nextInt();
+                if(kembaliMakanan == 0) continue;
+
+            }else if (pilihanPesanan == 2) {
+                System.out.println("\nDaftar Menu Minuman");
+                System.out.println("1. Americano\n2. Coffee Latte\n3. Sugar Latte");
+                System.out.println("Ketik '0' jika ingin melihat daftar makanan");
+                int kembaliMinuman;
+                kembaliMinuman = input.nextInt();
+                if(kembaliMinuman == 0) continue;
+            } else if (pilihanPesanan == 0){
+                System.out.println("Byee~");
+                daftarMenu = false;
+            } else {
+                System.out.print("Pilihan tidak ada, coba lagi");
+            }
+        }
     }
 }
--- P6_Muhammad_Fahim_Akbar/src/Source Package/Package/Latihan6.java
+++ P6_Muhammad_Fahim_Akbar/src/Source Package/Package/Latihan6.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class Latihan6 {
     public static void main(String[] args) {
+        int nim = 24;
 
+        if (nim == 30){
+            System.out.print("Selamat Anda Lulus dengan Nilai Sempurna");
+        } else {
+            System.out.print("Anda lulus dengan nilai standar");
+        }
     }
 }
