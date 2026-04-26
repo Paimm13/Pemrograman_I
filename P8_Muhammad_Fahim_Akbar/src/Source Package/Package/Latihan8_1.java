a23147334e363b6dbaad75c0445e4895833d8527
Paimm13 <fahim.akbar03pagi@gmail.com>
2026-04-26 23:54:51 +0700

    Pertemuan 8

--- P8_Muhammad_Fahim_Akbar/src/Source Package/Package/Latihan8_1.java
+++
@@ -1,8 +0,0 @@
-// PerulanganFor
-public class Latihan8_1 {
-    public static void main(String[] args){
-        for(int i=1; i <= 10; i++) {
-            System.out.println("Angka ke- "+i);
-        }
-    }
-}
